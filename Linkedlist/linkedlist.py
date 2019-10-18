a759b775e388ae875bf6e487bde0321e5a7c2363
Ajitesh Ranjan <ajiteshranjan22@gmail.com>
2019-10-18 18:39:28 +0530

    Initial commit

--- Linkedlist/linkedlist.py
+++ Linkedlist/linkedlist.py
@@ -1,1 +1,21 @@
+class Node:
+	def __init__(self,data):
+		self.data = data
+		self.next =None
 
+
+class LinkedList:
+	def __init__(self):
+		self.head = None
+
+	def insert(self, newNode):
+		if self.head is None:
+			self.head = newNode
+			
+		else:
+			lastNode = self.head
+			while True:
+				if lastNode.next is None:
+					break
+				lastNode = lastNode.next
+			lastNode.next = newNode
