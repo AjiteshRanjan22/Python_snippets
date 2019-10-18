cc7a35ebb13635544f1839ff4b1b875219acb747
Ajitesh Ranjan <ajiteshranjan22@gmail.com>
2019-10-18 21:56:56 +0530

    Initial commit

--- operator/meal.py
+++ operator/meal.py
@@ -1,1 +1,28 @@
+import math
+# import sys
 
+# input = sys.stdin.readline()
+def solve(meal_cost, tip_percent, tax_percent):
+	if meal_cost < 0 or tip_percent < 0 or tax_percent < 0:
+		print('Invalid entry: try again')
+		return False
+
+	totalMealCost = int(round(meal_cost + ((tip_percent/100)*meal_cost) + ((tax_percent/100)*meal_cost)))
+	print('totalMealCost is: ',totalMealCost)
+	
+bill = True
+while bill:
+	mealcost = float(input('Enter:'))
+	tippercent = int(input('Enter:'))
+	taxpercent = int(input('Enter:'))
+	totalcost = solve(mealcost, tippercent, taxpercent)
+	complete= False
+	if not complete:
+		again = input('do u have another bill: (y/n)')
+		if again.lower() == 'y':
+			print('Provide')
+		elif again.lower() == 'n':
+			bill = False
+		else:
+			print('Not a valid input')
+			bill = False
